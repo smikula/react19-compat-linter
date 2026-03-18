812207c5fa6e82de73927ff2fbb778c59a1b70d9
Scott Mikula <mikula@gmail.com>
2026-03-18 13:53:25 -0700

    Run webpack plugin in sample

--- sample/runSample.js
+++ sample/runSample.js
@@ -1,10 +1,41 @@
 const path = require('path');
+const webpack = require('webpack');
+const { DependencyModuleListPlugin } = require('../lib/index');
 const { runLinter } = require('../lib/runLinter');
 
-async function main() {
-    const modulesListPath = path.resolve(__dirname, 'modulesList.json');
+(async function main() {
+    console.log('Running webpack to generate module list...');
+    const modulesListPath = path.resolve(__dirname, 'dist', 'modulesList.json');
+    await runWebpack(modulesListPath);
+
+    console.log('Running linter...');
     const result = await runLinter(modulesListPath);
     console.log(JSON.stringify(result, null, 2));
-}
+})();
 
-main();
+function runWebpack(modulesListPath) {
+    return new Promise((resolve, reject) => {
+        const config = {
+            mode: 'production',
+            entry: path.resolve(__dirname, 'index.js'),
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'bundle.js',
+            },
+            plugins: [new DependencyModuleListPlugin(modulesListPath)],
+        };
+
+        webpack(config, (err, stats) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (stats.hasErrors()) {
+                    console.warn(
+                        '[webpack] Completed with errors:\n' + stats.toString('errors-only')
+                    );
+                }
+                resolve(stats);
+            }
+        });
+    });
+}
