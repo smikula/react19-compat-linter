d0e73c0b4d9a42d583e05e1c0df40f4afaff93de
Scott Mikula <mikula@gmail.com>
2026-03-18 14:01:18 -0700

    Run webpack plugin in sample (#23)
    Previously `yarn sample` just ran the linter on an existing module list.  This changes it to run end to end, running webpack with the plugin to generate the module list.

---
+++ sample/index.js
@@ -0,0 +1,5 @@
+// Webpack entry point for the sample
+// Imports from sample-package so the plugin collects those modules
+import 'sample-package/named-import';
+import 'sample-package/namespace-access';
+import 'sample-package/destructuring';
